d396bfa7971143ea4f68ec9033af4d45fe3e1768
Rakhitha Dharmasiri <rakhithaprabashwara@gmail.com>
2025-05-31 15:31:49 +0530

    Icon removed

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { FaTimes, FaBell, FaTicketAlt, FaTachometerAlt, FaUsers, FaBuilding, FaFileAlt, FaStar, FaUser, FaSignOutAlt } from 'react-icons/fa';
+import { FaTimes, FaBell, FaTicketAlt, FaTachometerAlt, FaUsers, FaBuilding, FaFileAlt, FaStar, FaSignOutAlt } from 'react-icons/fa';
 
 const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }) => {
     const navigate = useNavigate();
